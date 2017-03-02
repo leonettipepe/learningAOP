e04944e613a835a0d23474509b2ba8d6b0e380e4
leonettipepe <leonettipepe@gmail.com>
2017-03-02 19:30:58 +0500

    aopThroughAnnotation

--- src/main/java/logging/MyLogger.java
+++ src/main/java/logging/MyLogger.java
@@ -1,14 +1,23 @@
 package logging;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by leonid on 02.03.2017.
  */
 @Component
+@Aspect
 public class MyLogger {
 
+    @Pointcut(value="execution(* *(..))")
+    private void loggerPointCut() {
+    }
+
+    @Around("loggerPointCut()")
     public Object watchTime(ProceedingJoinPoint joinpoint) {
         long start = System.currentTimeMillis();
         System.out.println("method begin: " + joinpoint.getSignature().toShortString());
